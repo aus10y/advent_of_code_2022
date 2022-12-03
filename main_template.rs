0becbe2ff24ee87df97ed91bb05b7edc1f0b5419
Austin Young <email@austinyoung.me>
2022-12-02 22:37:33 -0600

    Added a template to use for each day's challenge.

---
+++ main_template.rs
@@ -0,0 +1,21 @@
+use std::io::{self, Read};
+
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    part2(&input)?;
+
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<()> {
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    Ok(())
+}
